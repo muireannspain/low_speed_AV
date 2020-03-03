8797dd413ed4c5994928786db919772011016451
wei0hu <59595440+wei0hu@users.noreply.github.com>
2020-03-02 16:17:30 -0800

    Update wp_test.m

--- catkin_ws/src/path_follower/src/wp_test.m
+++ catkin_ws/src/path_follower/src/wp_test.m
@@ -1,8 +1,8 @@
 % uiopen('EllipseWaypoints.csv');
-EllipseWaypoints1.Properties.VariableNames{1} = 'X';
-EllipseWaypoints1.Properties.VariableNames{2} = 'Y';
-x = EllipseWaypoints1.X;
-y = EllipseWaypoints1.Y;
+% EllipseWaypoints1.Properties.VariableNames{1} = 'X';
+% EllipseWaypoints1.Properties.VariableNames{2} = 'Y';
+x = EllipseWaypoints.X;
+y = EllipseWaypoints.Y;
 
 for i = 1:600
     plot(x(i),y(i),'*','color','r')
